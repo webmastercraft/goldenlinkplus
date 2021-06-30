f3d65144a49416ac98705016851b768b8d528e86
Iakov Agababian <iakov.agababian2020@gmail.com>
2021-06-30 20:30:12 +0300

    fix the issues for add more

--- src/components/Registration/Registration_five.vue
+++ src/components/Registration/Registration_five.vue
@@ -6,121 +6,121 @@
           <navbar :current=4></navbar>
           <Status :current=5></Status>
           <div class="content_register">
-            <form>
               <h6>Add Your Services</h6>
-              
-              <div class="form-group">
-                <select class="form-control" id="select-brand">
-                  <option>Select Brand</option>
-                  <option>2</option>
-                  <option>3</option>
-                  <option>4</option>
-                  <option>5</option>
-                </select>
-              </div>
-              <div class="form-group">
-                <select class="form-control" id="service_category">
-                  <option>Service Category</option>
-                  <option>2</option>
-                  <option>3</option>
-                  <option>4</option>
-                  <option>5</option>
-                </select>
-              </div>
-              <div class="form-group">
-                <select class="form-control" id="">
-                  <option></option>
-                  <option>2</option>
-                  <option>3</option>
-                  <option>4</option>
-                  <option>5</option>
-                </select>
-              </div>
-              <div class="form-group">
-                <select class="form-control" id="">
-                  <option></option>
-                  <option>2</option>
-                  <option>3</option>
-                  <option>4</option>
-                  <option>5</option>
-                </select>
-              </div>
-              <div class="form-group">
-                <input
-                  type="text"
-                  class="form-control"
-                  id="service_name"
-                  aria-describedby="service_name"
-                  placeholder="Service Name"
-                />
-              </div>
-              <div class="form-group">
-                <input
-                  type="text"
-                  class="form-control"
-                  id="price"
-                  aria-describedby="price"
-                  placeholder="Price"
-                />
-              </div>
-              <div class="form-group">
-                <input
-                  type="text"
-                  class="form-control"
-                  id="minimum quantity per order"
-                  aria-describedby="minimum quantity per order"
-                  placeholder="Minimum Quantity per Order"
-                />
-              </div>
-              <div class="form-group">
-                <input
-                  type="text"
-                  class="form-control"
-                  id="service_item"
-                  aria-describedby="service_item"
-                  placeholder="Service Item"
-                />
-              </div>
-              <div class="form-group wrapper">
-                <div class="box">
-                  <div class="js--image-preview"></div>
-                  <div class="upload-options">
-                    <label>
-                      <input type="file" class="image-upload" accept="image/*" />
-                    </label>
-                  </div>
+            <form>
+              <div v-for="index in service_count" :key="index">
+                <div class="form-group">
+                  <select class="form-control" id="select-brand">
+                    <option>Select Brand</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                  </select>
                 </div>
+                <div class="form-group">
+                  <select class="form-control" id="service_category">
+                    <option>Service Category</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                  </select>
+                </div>
+                <div class="form-group">
+                  <select class="form-control" id="">
+                    <option></option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                  </select>
+                </div>
+                <div class="form-group">
+                  <select class="form-control" id="">
+                    <option></option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                  </select>
+                </div>
+                <div class="form-group">
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="service_name"
+                    aria-describedby="service_name"
+                    placeholder="Service Name"
+                  />
+                </div>
+                <div class="form-group">
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="price"
+                    aria-describedby="price"
+                    placeholder="Price"
+                  />
+                </div>
+                <div class="form-group">
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="minimum quantity per order"
+                    aria-describedby="minimum quantity per order"
+                    placeholder="Minimum Quantity per Order"
+                  />
+                </div>
+                <div class="form-group">
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="service_item"
+                    aria-describedby="service_item"
+                    placeholder="Service Item"
+                  />
+                </div>
+                <div class="form-group wrapper">
+                  <div class="box">
+                    <div class="js--image-preview"></div>
+                    <div class="upload-options">
+                      <label>
+                        <input type="file" class="image-upload" accept="image/*" />
+                      </label>
+                    </div>
+                  </div>
 
-                <div class="box video">
-                  <div class="js--image-preview"></div>
-                  <div class="upload-options">
-                    <label>
-                      <input type="file" class="image-upload" accept="image/*" />
-                    </label>
+                  <div class="box video">
+                    <div class="js--image-preview"></div>
+                    <div class="upload-options">
+                      <label>
+                        <input type="file" class="image-upload" accept="image/*" />
+                      </label>
+                    </div>
                   </div>
                 </div>
+                <div class="form-group thumbnails">
+                  <div><img src=""></div>
+                  <div><img src=""></div>
+                  <div><img src=""></div>
+                  <div><img src=""></div>
+                  <div><img src=""></div>
+                </div>
+                <div class="form-group">
+                  <textarea
+                    class="form-control"
+                    id="description"
+                    rows="3"
+                    placeholder="Description"
+                  ></textarea>
+                </div>
+                <hr>
               </div>
-              <div class="form-group thumbnails">
-                <div><img src=""></div>
-                <div><img src=""></div>
-                <div><img src=""></div>
-                <div><img src=""></div>
-                <div><img src=""></div>
-              </div>
-              <div class="form-group">
-                <textarea
-                  class="form-control"
-                  id="description"
-                  rows="3"
-                  placeholder="Description"
-                ></textarea>
-              </div>
-              <hr>
-              <button type="submit" class="btn btn-primary next_btn add">Add more</button>
+              <button type="button" class="btn btn-primary next_btn add" @click="addService">Add more</button>
               <router-link to="/registration/six"  type="submit" class="btn btn-primary next_btn" tag="button">
                 Next
               </router-link>
-             
               <div class="icon_text">
                 <div class="later_icon">
                   <router-link to="/registration/six">
@@ -144,6 +144,16 @@ export default {
   components: {
     Navbar,
     Status
+  },
+  data() {
+    return {
+      service_count: 1,
+    }
+  },
+  methods: {
+    addService() {
+      this.service_count++;
+    }
   }
 }
 </script>
--- src/components/Registration/Registration_four.vue
+++ src/components/Registration/Registration_four.vue
@@ -6,117 +6,118 @@
           <navbar :current=3></navbar>
           <Status :current=4></Status>
           <div class="content_register">
+            <h6>Add Your Products</h6>
             <form>
-              <h6>Add Your Products</h6>
-              
-              <div class="form-group">
-                <select class="form-control" id="select-brand">
-                  <option>Select Brand</option>
-                  <option>2</option>
-                  <option>3</option>
-                  <option>4</option>
-                  <option>5</option>
-                </select>
-              </div>
-              <div class="form-group">
-                <select class="form-control" id="product_category">
-                  <option>Product Category</option>
-                  <option>2</option>
-                  <option>3</option>
-                  <option>4</option>
-                  <option>5</option>
-                </select>
-              </div>
-              <div class="form-group">
-                <select class="form-control" id="">
-                  <option></option>
-                  <option>2</option>
-                  <option>3</option>
-                  <option>4</option>
-                  <option>5</option>
-                </select>
-              </div>
-              <div class="form-group">
-                <select class="form-control" id="">
-                  <option></option>
-                  <option>2</option>
-                  <option>3</option>
-                  <option>4</option>
-                  <option>5</option>
-                </select>
-              </div>
-              <div class="form-group">
-                <input
-                  type="text"
-                  class="form-control"
-                  id="product name"
-                  aria-describedby="product name"
-                  placeholder="Product Name"
-                />
-              </div>
-              <div class="form-group">
-                <input
-                  type="text"
-                  class="form-control"
-                  id="price"
-                  aria-describedby="price"
-                  placeholder="Price"
-                />
-              </div>
-              <div class="form-group">
-                <input
-                  type="text"
-                  class="form-control"
-                  id="minimum quantity per order"
-                  aria-describedby="minimum quantity per order"
-                  placeholder="Minimum Quantity per Order"
-                />
-              </div>
-              <div class="form-group">
-                <input
-                  type="text"
-                  class="form-control"
-                  id="item_number"
-                  aria-describedby="item_number"
-                  placeholder="Item Number"
-                />
-              </div>
-              <div class="form-group wrapper">
-                <div class="box">
-                  <div class="js--image-preview"></div>
-                  <div class="upload-options">
-                    <label>
-                      <input type="file" class="image-upload" accept="image/*" />
-                    </label>
-                  </div>
+              <div v-for="index in products_count" :key="index">
+                <div class="form-group">
+                  <select class="form-control" id="select-brand">
+                    <option>Select Brand</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                  </select>
                 </div>
+                <div class="form-group">
+                  <select class="form-control" id="product_category">
+                    <option>Product Category</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                  </select>
+                </div>
+                <div class="form-group">
+                  <select class="form-control" id="">
+                    <option></option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                  </select>
+                </div>
+                <div class="form-group">
+                  <select class="form-control" id="">
+                    <option></option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                  </select>
+                </div>
+                <div class="form-group">
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="product name"
+                    aria-describedby="product name"
+                    placeholder="Product Name"
+                  />
+                </div>
+                <div class="form-group">
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="price"
+                    aria-describedby="price"
+                    placeholder="Price"
+                  />
+                </div>
+                <div class="form-group">
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="minimum quantity per order"
+                    aria-describedby="minimum quantity per order"
+                    placeholder="Minimum Quantity per Order"
+                  />
+                </div>
+                <div class="form-group">
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="item_number"
+                    aria-describedby="item_number"
+                    placeholder="Item Number"
+                  />
+                </div>
+                <div class="form-group wrapper">
+                  <div class="box">
+                    <div class="js--image-preview"></div>
+                    <div class="upload-options">
+                      <label>
+                        <input type="file" class="image-upload" accept="image/*" />
+                      </label>
+                    </div>
+                  </div>
 
-                <div class="box video">
-                  <div class="js--image-preview"></div>
-                  <div class="upload-options">
-                    <label>
-                      <input type="file" class="image-upload" accept="image/*" />
-                    </label>
+                  <div class="box video">
+                    <div class="js--image-preview"></div>
+                    <div class="upload-options">
+                      <label>
+                        <input type="file" class="image-upload" accept="image/*" />
+                      </label>
+                    </div>
                   </div>
                 </div>
+                <div class="form-group thumbnails">
+                  <div><img src=""></div>
+                  <div><img src=""></div>
+                  <div><img src=""></div>
+                  <div><img src=""></div>
+                  <div><img src=""></div>
+                </div>
+                <div class="form-group">
+                  <textarea
+                    class="form-control"
+                    id="description"
+                    rows="3"
+                    placeholder="Description"
+                  ></textarea>
+                </div>
+                <hr>
               </div>
-              <div class="form-group thumbnails">
-                <div><img src=""></div>
-                <div><img src=""></div>
-                <div><img src=""></div>
-                <div><img src=""></div>
-                <div><img src=""></div>
-              </div>
-              <div class="form-group">
-                <textarea
-                  class="form-control"
-                  id="description"
-                  rows="3"
-                  placeholder="Description"
-                ></textarea>
-              </div>
-              <hr>
-              <button type="submit" class="btn btn-primary next_btn add">Add more</button>
+              <button type="button" class="btn btn-primary next_btn add" @click="addProduct">Add more</button>
               <router-link to="/registration/five"  type="submit" class="btn btn-primary next_btn" tag="button">
                 Next
               </router-link>
@@ -144,6 +145,16 @@ export default {
   components: {
     Navbar,
     Status
+  },
+  data() {
+    return {
+      products_count: 1,
+    }
+  },
+  methods: {
+    addProduct() {
+      this.products_count++;
+    }
   }
 }
 </script>
