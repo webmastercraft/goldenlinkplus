5a9268670553174234314e31d0c0335b05d19839
Iakov Agababian <iakov.agababian2020@gmail.com>
2021-07-02 07:05:00 +0300

    complete demo of Certifications page

--- src/components/BusinessUserCenter/BUC_Certifications.vue
+++ src/components/BusinessUserCenter/BUC_Certifications.vue
@@ -8,16 +8,95 @@
             </div>
             
             <div class="account-title  marketing_plus">
-              Certifications<router-link to="/businessusercenter/certifications/edit"><i class="fas fa-plus-circle"></i></router-link>
+              Certifications<button @click="showEdit()" :disabled="isShow" :class="{disable: isShow === true}"><i class="fas fa-plus-circle"></i></button>
+            </div>
+            <div class="content_register" v-if="isShow === true">
+              <form>
+                <div>
+                  <div class="form-group">
+                    <select class="form-control" id="select-brand">
+                      <option disabled selected>Business Permit</option>
+                      <option>2</option>
+                      <option>3</option>
+                      <option>4</option>
+                      <option>5</option>
+                    </select>
+                  </div>
+                  <div class="form-group">
+                    <input
+                      type="text"
+                      class="form-control"
+                      id="document_name"
+                      aria-describedby="document name"
+                      placeholder="Document Name"
+                    />
+                  </div>
+                  <div class="form-group">
+                    <input
+                      type="text"
+                      class="form-control"
+                      id="register_number"
+                      aria-describedby="register_number"
+                      placeholder="Register Number"
+                    />
+                  </div>
+                  <div class="form-group">
+                    <input
+                      type="text"
+                      class="form-control"
+                      id="issued_by"
+                      aria-describedby="issued_by"
+                      placeholder="Issued By"
+                    />
+                  </div>
+                  <div class="form-group">
+                    <input
+                      type="text"
+                      class="form-control"
+                      id="expiry_date"
+                      aria-describedby="expiry_date"
+                      placeholder="Expiry Date"
+                    />
+                  </div>
+                  <div class="form-group wrapper">
+                    <div class="box">
+                      <div class="js--image-preview"></div>
+                      <div class="upload-options">
+                        <label>
+                          <input type="file" class="image-upload" accept="image/*" />
+                        </label>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="form-group thumbnails">
+                    <div><img src=""></div>
+                    <div><img src=""></div>
+                    <div><img src=""></div>
+                    <div><img src=""></div>
+                    <div><img src=""></div>
+                  </div>
+                  <div class="form-group">
+                    <textarea
+                      class="form-control"
+                      id="description"
+                      rows="3"
+                      placeholder="Description"
+                    ></textarea>
+                  </div>
+                </div>
+                <button type="button" class="btn btn_cancel" @click="hideEdit()">CANCEL</button>
+                <button type="button" class="btn btn_save " @click="saveCertifications()">SAVE</button>
+              </form>
+              <hr>
             </div>
             <div class="golden-area brands_top">
-              <p class="profile-title profile-title-buc">Document Type:<span> Business Permit</span></p>
-              <p class="profile-title profile-title-buc">Document Name:<span><br> CA Seller’s Permit</span></p>
-              <p class="profile-title profile-title-buc">Product Name:<span> GLP</span></p>
-              <p class="profile-title profile-title-buc">Registration Number:<span> 1234567</span></p>
-              <p class="profile-title profile-title-buc">Issued by:<span> Other Government Agency</span></p>
-              <p class="profile-title profile-title-buc">Date Issued:<span> 01/01/2013</span></p>
-              <p class="profile-title profile-title-buc">Expiry Date:<span> 01/01/2022</span></p>
+              <p class="profile-title profile-title-buc">Document Type:<span> {{document_type}}</span></p>
+              <p class="profile-title profile-title-buc">Document Name:<span><br> {{document_name}}</span></p>
+              <p class="profile-title profile-title-buc">Product Name:<span> {{product_name}}</span></p>
+              <p class="profile-title profile-title-buc">Registration Number:<span> {{registration_number}}</span></p>
+              <p class="profile-title profile-title-buc">Issued by:<span> {{issued}}</span></p>
+              <p class="profile-title profile-title-buc">Date Issued:<span> {{date_issued}}</span></p>
+              <p class="profile-title profile-title-buc">Expiry Date:<span> {{expiry_date}}</span></p>
               <p class="profile-title profile-title-buc">Uploaded Files:
               </p>
               <div class="form-group thumbnails upload_img">
@@ -27,7 +106,7 @@
                 <div><img src=""></div>
                 <div><img src=""></div>
               </div>
-              <p class="profile-title profile-title-buc">Description:<span><br> CA Seller’s Permit</span></p>
+              <p class="profile-title profile-title-buc">Description:<span><br> {{description}}</span></p>
             </div>
           </div>
         </div>
@@ -39,6 +118,31 @@
 export default {
   name: 'Certifications',
   components: {
+  },
+  data () {
+    return { 
+        isShow: false,
+        document_type: "Business Permit",
+        document_name: "CA Seller’s Permit",
+        product_name: "GLP",
+        registration_number: "1234567",
+        issued: "Other Government Agency",
+        date_issued: "01/01/2013",
+        expiry_date: "01/01/2022",
+        uploaded_files: [],
+        description: "CA Seller’s Permit",
+    }
+  },
+  methods: {
+    saveCertifications() {
+      this.hideEdit();
+    },
+    showEdit() {
+      this.isShow = true;
+    },
+    hideEdit() {
+      this.isShow = false;
+    }
   }
 }
 </script>
