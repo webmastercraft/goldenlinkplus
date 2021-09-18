c3af362f9954df6bfb7b15d0964655a4e020fdd6
Iakov Agababian <iakov.agababian2020@gmail.com>
2021-09-18 23:04:04 +0300

    update Event Qucik page

--- src/components/SocialMedia/SocialMedia_Main_Event_Quick.vue
+++ src/components/SocialMedia/SocialMedia_Main_Event_Quick.vue
@@ -1,10 +1,7 @@
 <template>
   <div>
     <div class="container">
-      <div class="loading_screen" v-show="isloading" >
-        <img src="loading_logo.png">
-      </div>
-      <div class="phone sociallogin" v-show="!isloading">
+      <div class="phone sociallogin">
         
           <div class="event_body_title">
             <p class="event_favicon_img"><img src="triangle.png">MAKING BIG TIME MONEY 101</p>
@@ -16,75 +13,22 @@
                 </router-link>
               <span @click="showEventSetting"><img src="img/dots.png"></span></p>
           </div>
-          <div class="event_body">
+          <div class="event_body event_quick_body">
             <div class="event_users">
                 <span class="blink_span">
                     <img v-if="isAddingCoin" class="blink-img"  :class="{'blinking': isBlink1 == true}" src="coin_10.png"  @click="showBlink1">
                     <img src="Jean.png" class="event_users_img" @click="showUserProfile">Jean
                 </span>
                 <span class="blink_span">
-                    <img v-if="isAddingCoin" class="blink-img"  :class="{'blinking': isBlink == true}" src="coin_10.png"  @click="showBlink">
-                    <img src="50k_coin.png" class="event_coin">
-                    <button>00:14:59</button>
                 </span>
                 <div class="event_para_group">
                     <p>
-                        <img src="user_count_grey.png">2501
-                        <img src="msg_count_grey.png">139
+                        <img src="user_count_grey.png">1
+                        <img src="msg_count_grey.png">0
                     </p>
-                    <p>13.9 K<img src="diamond.png"></p>
-                    <p @click="showPrice">625.4 K<img src="event_coin.png"></p>
-                    <p @click="showSupporter"><img src="login_users.png"></p>
+                    <p>0<img src="diamond_frame.png"></p>
+                    <p @click="showPrice">0<img src="event_coin.png"></p>
                 </div>
-            </div>
-            <div class="event_user_group">
-              <div v-for="(item, index) in datas" :key="index" class="event_each_user">
-                <div class="event_icon">
-                    <img v-if="isAddingCoin" class="blink-img"  :class="{'blinkingUser': item.blink == true}" src="coin_10.png"  @click="showBlinkItem(index)">
-                    <img v-if="!isAddingCoin" class="diamond-img"  :class="{'diamondAppear': item.diamondActive == true}" src="frame_diamond2.png" @dblclick="showDiamondItem(index)">
-                    <img :src="`${item.image_url}`" :class="{'user_logo_border': item.active == true}">
-                    <div>
-                        <img src="pink_star.png" v-if="item.star" class="event_star">
-                        <img src="mute.png" v-if="item.mute" class="event_mute">
-                        <img src="diamond.png" v-if="item.diamond" class="event_diamond">
-                    </div>
-                </div>
-                <p class="coin_letter"><img src="event_user_coin.png" class="event_user_coin">{{item.event_user_coin}}K</p>
-                <p>{{item.name}}</p>
-              </div>
-            </div>
-            <div class="event_ranking">
-              <button class="audience_btn" :class="{'border_btn': isAudience == true}" @click="showAudience"><b>Audience</b></button>
-              <button class="speakers_btn" :class="{'border_btn': isAudience != true}" @click="showSpeakers"><b>Speakers</b></button>
-            </div>
-
-            <div v-if="isAudience == true" class="coin_letter">
-            <p class="event_ranking_title">Top 18 GCoins sent by audience</p>
-            </div>
-            <div v-else class="coin_letter">
-            <p class="event_ranking_title">Top 5 GCoins sent by speakers</p>
-            </div>
-
-            <div v-if="isAudience == true">
-              <div v-for="(item, index) in rangking1" :key="index" class="rangking_data">
-                <img :src="`${item.rangking_image}`">
-                <p>{{item.rangking_name}}<span>{{item.rangking_coin}}</span></p>
-              </div>
-            </div>
-            <div v-else>
-              <div v-for="(item, index) in rangking2" :key="index" class="rangking_data">
-                <img :src="`${item.rangking_image}`">
-                <p>{{item.rangking_name}}<span>{{item.rangking_coin}}</span></p>
-              </div>
-            </div>
-            <div class="event_mute_group">
-              <div v-for="(item, index) in mutedata" :key="index" class="mute_user">
-                
-                <div class="mute_icon">
-                  <img :src="`${item.mute_image}`" class="mute_user_img">
-                </div>
-                <p>{{item.mute_user}}</p>
-              </div>
             </div>
           </div>
           <div class="footer_background">
@@ -195,7 +139,6 @@ export default {
         isBlink: false,
         isDiamond: false,
         isBlink1: false,
-        isloading: true,
         isModalVisible: false,
         clicked: false,
         imgClicked: false,
@@ -208,308 +151,6 @@ export default {
         f_show_event_setting: false,
         f_show_upcoming_event: false,
         modalStack: [],
-        datas: [
-            {
-              image_url: "shane.png",
-              star: true,
-              mute: true,
-              diamond: true,
-              active: false,
-              blink: false,
-              event_user_coin: 892,
-              name: "Shane",
-              diamondActive: false
-            },
-            {
-              image_url: "eduardo.png",
-              star: false,
-              mute: false,
-              diamond: true,
-              active: true,
-              blink: false,
-              event_user_coin: 484.2,
-              name: "Kristin",
-              diamondActive: false
-            },
-            {
-              image_url: "leslie.png",
-              star: true,
-              mute: true,
-              diamond: true,
-              active: false,
-              blink: false,
-              event_user_coin: 345.6,
-              name: "Leslie",
-              diamondActive: false
-            },
-            {
-              image_url: "eduardo.png",
-              star: true,
-              mute: true,
-              diamond: true,
-              active: false,
-              blink: false,
-              event_user_coin: 527.1,
-              name: "Eduardo",
-              diamondActive: false
-            },
-            {
-              image_url: "eduardo.png",
-              star: false,
-              mute: true,
-              diamond: true,
-              active: true,
-              blink: false,
-              event_user_coin: 78.5,
-              name: "Jorge",
-              diamondActive: false
-            },
-            {
-              image_url: "esther.png",
-              star: true,
-              mute: true,
-              diamond: true,
-              active: false,
-              blink: false,
-              event_user_coin: 18.9,
-              name: "Esther",
-              diamondActive: false
-            },
-            {
-              image_url: "leslie.png",
-              star: false,
-              mute: true,
-              diamond: true,
-              active: false,
-              blink: false,
-              event_user_coin: 27.5,
-              name: "Tanya",
-              diamondActive: false
-            },
-            {
-              image_url: "shane.png",
-              star: false,
-              mute: true,
-              diamond: true,
-              active: false,
-              blink: false,
-              event_user_coin: 38.8,
-              name: "Ronald",
-              diamondActive: false
-            },
-            {
-              image_url: "esther.png",
-              star: false,
-              mute: true,
-              diamond: true,
-              active: false,
-              blink: false,
-              event_user_coin: 190.5,
-              name: "Philip",
-              diamondActive: false
-            },
-        ],
-        rangking2: [
-            {
-              rangking_image: "Ray.png",
-              rangking_name: "Virginia Jones",
-              rangking_coin: "915.6K",
-            },
-            {
-              rangking_image: "mona.png",
-              rangking_name: "Lavern Laboy",
-              rangking_coin: "847.3K",
-            },
-            {
-              rangking_image: "Jean_Smith.png",
-              rangking_name: "Annette Black",
-              rangking_coin: "654.2K",
-            },
-            {
-              rangking_image: "Richard_Bennett_off.png",
-              rangking_name: "Darron Kowski",
-              rangking_coin: "302.8K",
-            },
-            {
-              rangking_image: "Susan Boyle.png",
-              rangking_name: "Robert Fox",
-              rangking_coin: "113.3K",
-            },
-        ],
-        rangking1: [
-            {
-              rangking_image: "Ray.png",
-              rangking_name: "Erin Hughes",
-              rangking_coin: "876.5K",
-            },
-            {
-              rangking_image: "mona.png",
-              rangking_name: "Marvin McKinney",
-              rangking_coin: "678.6K",
-            },
-            {
-              rangking_image: "Jean_Smith.png",
-              rangking_name: "Lucy Fletcher",
-              rangking_coin: "582.3K",
-            },
-            {
-              rangking_image: "Richard_Bennett_off.png",
-              rangking_name: "Ronald Richards",
-              rangking_coin: "76.5K",
-            },
-            {
-              rangking_image: "Susan Boyle.png",
-              rangking_name: "Kathryn Murphy",
-              rangking_coin: "24K",
-            },
-            {
-              rangking_image: "Alex Smith.png",
-              rangking_name: "Emily Malone",
-              rangking_coin: "18.5K",
-            },
-            {
-              rangking_image: "Collen.png",
-              rangking_name: "Patricia Clark",
-              rangking_coin: "12.9K",
-            },
-            {
-              rangking_image: "Grandma.png",
-              rangking_name: "Jerome Bell",
-              rangking_coin: "6.7K",
-            },
-            {
-              rangking_image: "Irma.png",
-              rangking_name: "Floyd Miles",
-              rangking_coin: "5.9K",
-            },
-            {
-              rangking_image: "Lily.png",
-              rangking_name: "Mary Lopez",
-              rangking_coin: "3.6K",
-            },
-            {
-              rangking_image: "Kathry.png",
-              rangking_name: "Amanda Rice",
-              rangking_coin: "3.2K",
-            },
-            {
-              rangking_image: "Judith.png",
-              rangking_name: "Lydia Sharp",
-              rangking_coin: "1.1K",
-            },
-            {
-              rangking_image: "Priscilla.png",
-              rangking_name: "Darrell Steward",
-              rangking_coin: "546",
-            },
-            {
-              rangking_image: "Serenity.png",
-              rangking_name: "Ida Baldwin",
-              rangking_coin: "404",
-            },
-            {
-              rangking_image: "Savannah.png",
-              rangking_name: "Jacob Jones",
-              rangking_coin: "395",
-            },
-            {
-              rangking_image: "Wendy.png",
-              rangking_name: "Linda Carroll",
-              rangking_coin: "375",
-            },
-            {
-              rangking_image: "Susan Boyle.png",
-              rangking_name: "Ruth West",
-              rangking_coin: "210",
-            },
-            {
-              rangking_image: "Philip.png",
-              rangking_name: "Cody Fisher",
-              rangking_coin: "75",
-            },
-        ],
-        mutedata: [
-            {
-              mute_image: "Irma.png",
-              mute_user: "Pat",
-            },
-            {
-              mute_image: "Wendy.png",
-              mute_user: "Wendy",
-            },
-            {
-              mute_image: "Irma.png",
-              mute_user: "Tanya",
-            },
-            {
-              mute_image: "Wendy.png",
-              mute_user: "Marvin",
-            },
-            {
-              mute_image: "Irma.png",
-              mute_user: "Ronald",
-            },
-            {
-              mute_image: "Wendy.png",
-              mute_user: "Cameron",
-            },
-            {
-              mute_image: "Irma.png",
-              mute_user: "Brandon",
-            },
-            {
-              mute_image: "Wendy.png",
-              mute_user: "Randall",
-            },
-            {
-              mute_image: "Irma.png",
-              mute_user: "Jorge",
-            },
-            {
-              mute_image: "Wendy.png",
-              mute_user: "Dustin",
-            },
-            {
-              mute_image: "Irma.png",
-              mute_user: "Regina",
-            },
-            {
-              mute_image: "Wendy.png",
-              mute_user: "Gloria",
-            },
-            {
-              mute_image: "Irma.png",
-              mute_user: "Harold",
-            },
-            {
-              mute_image: "Wendy.png",
-              mute_user: "Francisco",
-            },
-            {
-              mute_image: "Irma.png",
-              mute_user: "Julie",
-            },
-            {
-              mute_image: "Wendy.png",
-              mute_user: "Savannah",
-            },
-            {
-              mute_image: "Wendy.png",
-              mute_user: "Calvin",
-            },
-            {
-              mute_image: "Irma.png",
-              mute_user: "Dwight",
-            },
-            {
-              mute_image: "Wendy.png",
-              mute_user: "Ann",
-            },
-            {
-              mute_image: "Irma.png",
-              mute_user: "Arlene",
-            },
-        ]
     }
   },
   
@@ -520,22 +161,6 @@ export default {
     },
     doneAddingCoin() {
         this.isAddingCoin = false;
-    },
-    showBlinkItem(index) {
-        if (this.isAddingCoin) {
-            this.datas[index].blink = true
-            setTimeout(() => {
-                this.datas[index].blink = false
-            }, 2000)
-        }
-    },
-    showDiamondItem(index) {
-        if (!this.isAddingCoin) {
-            this.datas[index].diamondActive = true
-            setTimeout(() => {
-                this.datas[index].diamondActive = false
-            }, 2000)
-        }
     },
     showBlink() {
         if (this.isAddingCoin) {
@@ -553,11 +178,6 @@ export default {
             }, 2000)
         } 
     },
-    killLoading() {
-      setTimeout(() => {
-          this.isloading = false
-      }, 2000)
-    },
     showIt() {
         Vuedals.$emit('new', {
             name: 'showing-the-money',
@@ -574,12 +194,6 @@ export default {
               }
             });
     },
-    showAudience() {
-      this.isAudience = true;
-    },
-    showSpeakers() {
-      this.isAudience = false;
-    },
     showModal() {
       this.isModalVisible = true;
     },
@@ -590,7 +204,6 @@ export default {
       this.isModalVisible = false;
     },
     removeFlagFromStack() {
-
       let temp = this.modalStack.pop(-1);
 
       switch (temp) {
@@ -600,33 +213,15 @@ export default {
         case 'f_show_user_profile':
           this.f_show_user_profile = false
           break;
-        default:
-          // statements_def
-          break;
-      }
-      switch (temp) {
         case 'f_show_invite':
           this.f_show_invite = false
           break;
-        default:
-          // statements_def
-          break;
-      }
-      switch (temp) {
         case 'f_show_price':
           this.f_show_price = false
           break;
-        default:
-          break;
-      }
-      switch (temp) {
         case 'f_show_supporter':
           this.f_show_supporter = false
           break;
-        default:
-          break;
-      }
-      switch (temp) {
         case 'f_show_event_setting':
           this.f_show_event_setting = false
           break;
@@ -687,21 +282,18 @@ export default {
     closeUpcomingEvent() {
       this.f_show_upcoming_event = false;
     },
-
-
   },
   computed: {
     imgSrc: function () {
       return this.imgClicked ? 'figure_orange.png' : 'figure.png'
     },
-  },
-  created () {
-    this.killLoading();
-  },
+  }
 }
 </script>
 
 <style>
-  
+  .event_quick_body {
+    margin-bottom: 110px;
+  }
 </style>
  
