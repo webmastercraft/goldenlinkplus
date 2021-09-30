2c5bee40866542bebdefaae0bd6a84250647f0ed
DESKTOP-LF34OKH\snoopy <iakov.agababian2020@gmail.com>
2021-09-29 21:44:22 -0700

    fix the button size

--- src/components/SocialMedia/SocialMedia_Reg_verify.vue
+++ src/components/SocialMedia/SocialMedia_Reg_verify.vue
@@ -14,7 +14,11 @@
             placeholder=""
           />
           <p class="glplogo_reg_des">Enter code in 180s<span><router-link to="/socialmedia/socialmedia_reg_security" class="glplogo_reg_code">Resend Code</router-link></span></p>
-          <div class="sociallogo_btn"><button><router-link to="/socialmedia/socialmedia_reg_account_option" class="glplogo_reg_btn">Next <img class="glplogo_reg_btn_img" src="reg_next.png"/></router-link></button>
+          <div class="sociallogo_btn">
+            <router-link to="/socialmedia/socialmedia_reg_account_option">
+              <button class="glplogo_reg_btn">Next <img class="glplogo_reg_btn_img" src="reg_next.png"/>
+              </button>
+            </router-link>
           </div>
         </div>
       </div>
